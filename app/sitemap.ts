d14d3aed2747757196e6a0f642459dd3585538ca
Bassilio <bassilede88@gmail.com>
2026-01-21 13:15:25 +0100

    Fix sitemap canonical domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,8 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.brightway2deutschland.com";
+const baseUrl = "https://brightway2deutschland.com";
+
 
   return [
     {
