422c1948acc4e494eea83e0a18ea1eab1900f599
Bassilio <bassilede88@gmail.com>
2026-01-30 09:05:54 +0100

    google no longer needs to: choose different cononical than the user

--- app/de/layout.tsx
+++ app/de/layout.tsx
@@ -2,19 +2,28 @@ import Navbar from "../components/Navbar";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
+  title: {
+    default: "Brightway2Deutschland | Ausbildung Consulting",
+    template: "%s | Brightway2Deutschland",
+  },
   alternates: {
-    canonical: "https://brightway2deutschland.com/de",
+    canonical: "https://brightway2deutschland.com/en",
+    languages: {
+      de: "https://brightway2deutschland.com/de",
+      en: "https://brightway2deutschland.com/en",
+      "x-default": "https://brightway2deutschland.com/",
+    },
   },
 };
 
-export default function DeLayout({
+export default function EnLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <>
-      <Navbar locale="de" />
+      <Navbar locale="en" />
       {children}
     </>
   );
